6fd2f8ef1e74f296884215017068753d81c11b69
IvanSemerenko98 <ivansemerenko@suuchi.com>
2020-03-19 23:13:23 -0400

    styled-components added

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled, {css} from 'styled-components';
+
 
 class App extends Component {
   render(){
     return (
       <div>
-        Hello World!
-      </div>
+        {/* <Welcome name={"Crypto Dash"}/> */}
+     </div>
     );
   }
 }
