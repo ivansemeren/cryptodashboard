1b2ea6c44ef3c7368edf48d9c8f5e60ac7393b8f
IvanSemerenko98 <ivansemerenko@suuchi.com>
2020-03-28 00:45:47 -0400

    Bug Fixed

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -14,6 +14,10 @@ const ControlButtonElem = styled.div`
     ${props => props.active && css `
         text-shadow: 0px 0px 60px #03ff03;
     `}
+    ${props => props.hidden && css `
+        display: none;
+    `}
+
 `
 function toProperCase(lower){
     return lower.charAt(0).toUpperCase() + lower.substr(1);
@@ -21,8 +25,11 @@ function toProperCase(lower){
 
 function ControlButton({name, active}){
     return (
-        <AppContext.Consumer>{({page, setPage})=> (
-            <ControlButtonElem active={page === name} onClick={()=>setPage(name)}>
+        <AppContext.Consumer>{({page, setPage, firstVisit})=> (
+            <ControlButtonElem 
+                active={page === name} 
+                onClick={()=>setPage(name)}
+                hidden={firstVisit && name === 'dashboard'}>
                 {toProperCase(name)}
             </ControlButtonElem>
         )}
